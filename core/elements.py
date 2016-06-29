de0ad8edf3dc24cec19751c1a5777903559b6ce6
Harmandeep Singh <harmandeep.singh1@delhivery.com>
2016-06-29 16:44:36 +0530

    Basic Graph elememts added

---
+++ core/elements.py
@@ -0,0 +1,45 @@
+"""
+This module contains the basic elements that comprise the Game
+
+All the elements that the game will contain will be part of 
+this module
+
+@author: Harmandeep Singh
+@email:  harmandeep3091 at gmail dot com
+@date:   2016-06-29 Wed
+"""
+
+
+class Player(object):
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Country(object):
+
+    def __init__(self, name, continent):
+        self.name = name
+        self.continent = continent
+
+
+class Map(object):
+
+    def __init__(self, graph_list, index_country):
+        """
+        graph_list -- dict containing `key` as unique index of node in
+                      `index_country_dict` and value will be list of 
+                      indexes of adjacent nodes.
+                      The main use of this dict is to create the graph
+
+        index_country -- list containing `elements.Country` object
+                         in the order of indexes required in 
+                         `graph_list`
+
+        """
+        import copy
+        self.graph_dict = copy.deepcopy(graph_dict)
+        self.index_country_map = copy.deepcopy(index_country_map)
+        self.country_name_index_map = {}
+        for idx, each_country in enumerate(index_country_dict):
+            self.name_index_map[each_country.name] = idx
